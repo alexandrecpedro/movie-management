33adf7cf64dcbecdede0f110f717cf75528c241f
alexandrecpedro <alexandrecpedro@gmail.com>
2025-03-27 22:07:59 -0300

    feat: main menu

--- DELETE.cob
+++ DELETE.cob
@@ -11,105 +11,107 @@
 
        SPECIAL-NAMES.
            DECIMAL-POINT       IS COMMA.
-       
+
        INPUT-OUTPUT            SECTION.
        FILE-CONTROL.
-             SELECT MOVIES ASSIGN TO "./Data/MOVIES.DAT"
+      *       SELECT MOVIES ASSIGN TO "./Data/MOVIES.DAT"
+             SELECT MOVIES ASSIGN TO "C:\Cobol\MOVIES.DAT"
              ORGANIZATION IS INDEXED
-             ACCESS MODE IS RANDOM
+             ACCESS MODE IS DYNAMIC
              FILE STATUS IS MOVIES-STATUS
              RECORD KEY IS MOVIES-KEY.
 
        DATA                    DIVISION.
        FILE                    SECTION.
-      *---------------------------- FILE DESCRIPTION 
+      *---------------------------- FILE DESCRIPTION
        FD MOVIES.
        01 MOVIES-REG.
-            05 MOVIES-KEY           PIC 9(005).
-            05 MOVIES-TITLE         PIC X(050).
-            05 MOVIES-GENRE         PIC X(030).
-            05 MOVIES-DURATION      PIC 9(003).
-            05 MOVIES-DISTRIBUTOR   PIC X(040).
-            05 MOVIES-RATING        PIC 9(002).
+            05 MOVIES-KEY            PIC 9(005).
+            05 MOVIES-TITLE          PIC X(050).
+            05 MOVIES-GENRE          PIC X(030).
+            05 MOVIES-DURATION       PIC 9(003).
+            05 MOVIES-DISTRIBUTOR    PIC X(040).
+            05 MOVIES-RATING         PIC 9(002).
 
 
        WORKING-STORAGE         SECTION.
       *---------------------------- DATA ENTRY VARIABLES
        77 WRK-KEY                    PIC X(001).
-      
-      *---------------------------- FILE
-       77 MOVIES-STATUS             PIC 9(02) VALUE ZEROS.
 
       *---------------------------- ERROR MESSAGES
-       77 WRK-ERROR-MSG             PIC X(030) VALUE SPACES.
+       77 WRK-ERROR-MSG              PIC X(030) VALUE SPACES.
 
        01   WRK-MSGS.
-            05  WRK-MSG-CORRUPTED   PIC X(030) VALUE
+            05  WRK-MSG-CORRUPTED    PIC X(030) VALUE
             "CORRUPTED FILE".
             05  WRK-MSG-NOTDELETED   PIC X(030) VALUE
             "ERROR WHILE DELETING ENTITY".
-            05  WRK-MSG-NOTFOUND    PIC X(030) VALUE
+            05  WRK-MSG-NOTFOUND     PIC X(030) VALUE
             "ENTITY NOT FOUND".
-            05  WRK-MSG-OPEN        PIC X(030) VALUE
+            05  WRK-MSG-OPEN         PIC X(030) VALUE
             "ERROR WHILE OPENING FILE".
-            05  WRK-MSG-PATH        PIC X(030) VALUE
+            05  WRK-MSG-PATH         PIC X(030) VALUE
             "ERROR ON FILE PATH".
-            05  WRK-MSG-UNKNOWN     PIC X(030) VALUE
+            05  WRK-MSG-UNKNOWN      PIC X(030) VALUE
             "UNKNOWN ERROR".
-       
+
+      *---------------------------- FILE
+       77 MOVIES-STATUS              PIC 9(02) VALUE ZEROS.
+
        LINKAGE                 SECTION.
       *---------------------------- LINKAGE VARIABLES
        01 LNK-TITLE.
-            05 LNK-SCREEN-TITLE     PIC X(020).
-            05 LNK-MODULE-TITLE     PIC X(016).
+            05 LNK-SCREEN-TITLE      PIC X(020).
+            05 LNK-MODULE-TITLE      PIC X(016).
 
        SCREEN                  SECTION.
-      *---------------------------- SCREEN LAYOUT
-       01 CLEANER-SCREEN.
-            05 CLEAN-SCREEN.
-               10 BLANK SCREEN.
-               10 LINE 01 COLUMN 01 PIC X(020) ERASE EOL
-                    BACKGROUND-COLOR 3.
-               10 LINE 01 COLUMN 15 PIC X(020)
-                    BACKGROUND-COLOR 3 FOREGROUND-COLOR 0
-                    FROM LNK-SCREEN-TITLE.
-               10 LINE 02 COLUMN 01 PIC X(025) ERASE EOL
-                    BACKGROUND-COLOR 1.
-               10 LINE 02 COLUMN 14 PIC X(015)
-                    BACKGROUND-COLOR 1 FOREGROUND-COLOR 4
-                    FROM LNK-MODULE-TITLE.
-
-      *---------------------------- MOVIE DATA SCREEN
-       01 ENTITY-DATA-SCREEN.
-            05 QUERY-KEY FOREGROUND-COLOR 2.
-                10 LINE 10 COLUMN 10 VALUE "ID: ".
-                10 COLUMN PLUS 2    PIC 9(005) USING MOVIES-KEY
-                   BLANK WHEN ZEROS.
-            05 ENTITY-DATA.
-                10 LINE 11 COLUMN 10 VALUE "TITLE: ".
-                10 COLUMN PLUS 2    PIC X(050) USING MOVIES-TITLE.
-                10 LINE 12 COLUMN 10 VALUE "GENRE: ".
-                10 COLUMN PLUS 2    PIC X(030) USING MOVIES-GENRE.
-                10 LINE 13 COLUMN 10 VALUE "DURATION: ".
-                10 COLUMN PLUS 2    PIC 9(003) USING MOVIES-DURATION
-                   BLANK WHEN ZEROS.
-                10 LINE 14 COLUMN 10 VALUE "DISTRIBUTOR: ".
-                10 COLUMN PLUS 2    PIC X(040) USING MOVIES-DISTRIBUTOR.
-                10 LINE 15 COLUMN 10 VALUE "RATING: ".
-                10 COLUMN PLUS 2    PIC 9(002) USING MOVIES-RATING
-                   BLANK WHEN ZEROS.
-
       *---------------------------- ERROR SCREEN
        01 ERROR-SCREEN.
             05 MSG-ERROR.
                 10 LINE 16 COLUMN 01 ERASE EOL
                    BACKGROUND-COLOR 3.
-                10 LINE 16 COLUMN 10 PIC X(030) 
-                   BACKGROUND-COLOR 3 
+                10 LINE 16 COLUMN 10 PIC X(030)
+                   BACKGROUND-COLOR 3
                    USING WRK-ERROR-MSG.
-                10 COLUMN PLUS 2     PIC X(001) 
+                10 COLUMN PLUS 2     PIC X(001)
                    BACKGROUND-COLOR 3
                    USING WRK-KEY.
+
+      *---------------------------- MOVIE DATA SCREEN
+       01 ENTITY-DATA-SCREEN.
+            05 QUERY-KEY FOREGROUND-COLOR 2.
+                10 LINE 10 COLUMN 10 VALUE "ID: ".
+                10 COLUMN PLUS 2     PIC 9(005) USING MOVIES-KEY
+                   BLANK WHEN ZEROS.
+            05 ENTITY-DATA.
+                10 LINE 11 COLUMN 10 VALUE "TITLE: ".
+                10 COLUMN PLUS 2     PIC X(050) USING MOVIES-TITLE.
+                10 LINE 12 COLUMN 10 VALUE "GENRE: ".
+                10 COLUMN PLUS 2     PIC X(030) USING MOVIES-GENRE.
+                10 LINE 13 COLUMN 10 VALUE "DURATION: ".
+                10 COLUMN PLUS 2     PIC 9(003) USING MOVIES-DURATION
+                   BLANK WHEN ZEROS.
+                10 LINE 14 COLUMN 10 VALUE "DISTRIBUTOR: ".
+                10 COLUMN PLUS 2     PIC X(040)
+                   USING MOVIES-DISTRIBUTOR.
+                10 LINE 15 COLUMN 10 VALUE "RATING: ".
+                10 COLUMN PLUS 2     PIC 9(002) USING MOVIES-RATING
+                   BLANK WHEN ZEROS.
+
+      *---------------------------- SCREEN LAYOUT
+       01 CLEANER-SCREEN.
+            05 CLEAN-SCREEN.
+               10 BLANK SCREEN.
+               10 LINE 01 COLUMN 01  PIC X(020) ERASE EOL
+                    BACKGROUND-COLOR 3.
+               10 LINE 01 COLUMN 15  PIC X(020)
+                    BACKGROUND-COLOR 3 FOREGROUND-COLOR 0
+                    FROM LNK-SCREEN-TITLE.
+               10 LINE 02 COLUMN 01  PIC X(025) ERASE EOL
+                    BACKGROUND-COLOR 1.
+               10 LINE 02 COLUMN 14  PIC X(015)
+                    BACKGROUND-COLOR 1 FOREGROUND-COLOR 4
+                    FROM LNK-MODULE-TITLE.
 
        PROCEDURE               DIVISION USING LNK-TITLE.
 
@@ -124,7 +126,7 @@
             PERFORM 0210-VERIFICATION.
 
        0210-VERIFICATION       SECTION.
-       0220-VERIFICATION-MOVIES.
+       0201-VERIFICATION-MOVIES.
             EVALUATE MOVIES-STATUS
               WHEN 0
                 CONTINUE
@@ -138,25 +140,32 @@
                 MOVE WRK-MSG-UNKNOWN TO WRK-ERROR-MSG
             END-EVALUATE.
 
-            PERFORM 9000-MANAGE-ERROR.
+            IF WRK-ERROR-MSG NOT EQUAL SPACES
+               PERFORM 9000-MANAGE-ERROR
+            END-IF.
 
        0300-PROCESS            SECTION.
+            MOVE SPACES TO MOVIES-TITLE MOVIES-GENRE MOVIES-DISTRIBUTOR
+               WRK-ERROR-MSG WRK-KEY.
+            MOVE ZEROS TO MOVIES-KEY MOVIES-DURATION MOVIES-RATING.
+
             DISPLAY CLEANER-SCREEN.
             DISPLAY ENTITY-DATA-SCREEN.
             ACCEPT QUERY-KEY.
-            PERFORM 0310-READ THRU 0320-DELETE.
+            PERFORM 0310-READ.
+            PERFORM 0320-DELETE.
 
-       0310-READ.
+       0310-READ               SECTION.
             READ MOVIES
                INVALID KEY
                    MOVE WRK-MSG-NOTFOUND TO WRK-ERROR-MSG
                NOT INVALID KEY
                    MOVE "DO YOU WANT TO PROCEED (Y/N)?" TO WRK-ERROR-MSG
-                   DISPLAY ENTITY-DATA  
+                   DISPLAY ENTITY-DATA
             END-READ.
             PERFORM 9000-MANAGE-ERROR.
-       
-       0320-DELETE.
+
+       0320-DELETE             SECTION.
             IF WRK-KEY = 'Y' AND MOVIES-STATUS = 0
                DELETE MOVIES
                    INVALID KEY
@@ -168,6 +177,6 @@
        0400-FINALIZE           SECTION.
            CLOSE MOVIES.
            GOBACK.
-       
+
        9000-MANAGE-ERROR       SECTION.
             ACCEPT ERROR-SCREEN.
--- MENU.cob
+++ MENU.cob
@@ -11,40 +11,33 @@
 
        SPECIAL-NAMES.
            DECIMAL-POINT       IS COMMA.
-       
+
        INPUT-OUTPUT            SECTION.
        FILE-CONTROL.
-             SELECT MOVIES ASSIGN TO "./Data/MOVIES.DAT"
+      *       SELECT MOVIES ASSIGN TO "./Data/MOVIES.DAT"
+             SELECT MOVIES ASSIGN TO "C:\Cobol\MOVIES.DAT"
              ORGANIZATION IS INDEXED
-             ACCESS MODE IS RANDOM
+             ACCESS MODE IS DYNAMIC
              FILE STATUS IS MOVIES-STATUS
              RECORD KEY IS MOVIES-KEY.
 
        DATA                    DIVISION.
        FILE                    SECTION.
-      *---------------------------- FILE DESCRIPTION 
+      *---------------------------- FILE DESCRIPTION
        FD MOVIES.
        01 MOVIES-REG.
-            05 MOVIES-KEY           PIC 9(005).
-            05 MOVIES-TITLE         PIC X(050).
-            05 MOVIES-GENRE         PIC X(030).
-            05 MOVIES-DURATION      PIC 9(003).
-            05 MOVIES-DISTRIBUTOR   PIC X(040).
-            05 MOVIES-RATING        PIC 9(002).
+            05 MOVIES-KEY            PIC 9(005).
+            05 MOVIES-TITLE          PIC X(050).
+            05 MOVIES-GENRE          PIC X(030).
+            05 MOVIES-DURATION       PIC 9(003).
+            05 MOVIES-DISTRIBUTOR    PIC X(040).
+            05 MOVIES-RATING         PIC 9(002).
 
 
        WORKING-STORAGE         SECTION.
       *---------------------------- DATA ENTRY VARIABLES
        77 WRK-OPTION                 PIC X(001).
        77 WRK-KEY                    PIC X(001).
-
-      *---------------------------- TITLES
-       01 WRK-TITLE.
-            05 WRK-SCREEN-TITLE      PIC X(020) VALUE "MOVIE SYSTEM".
-            05 WRK-MODULE-TITLE      PIC X(016) VALUE SPACES.
-
-      *---------------------------- FILE
-       77 MOVIES-STATUS             PIC 9(02) VALUE ZEROS.
 
       *---------------------------- ERROR MESSAGES
        77 WRK-ERROR-MSG              PIC X(030) VALUE SPACES.
@@ -58,6 +51,14 @@
             "INVALID OPTION! TRY AGAIN".
             05  WRK-MSG-PATH         PIC X(030) VALUE
             "ERROR ON FILE PATH".
+
+      *---------------------------- FILE
+       77 MOVIES-STATUS              PIC 9(02) VALUE ZEROS.
+
+      *---------------------------- TITLES
+       01 WRK-TITLE.
+            05 WRK-SCREEN-TITLE      PIC X(020) VALUE "MOVIE SYSTEM".
+            05 WRK-MODULE-TITLE      PIC X(016) VALUE SPACES.
 
        SCREEN                  SECTION.
       *---------------------------- SCREEN LAYOUT
@@ -85,16 +86,16 @@
             05 LINE 12 COLUMN 15 VALUE "X - EXIT".
             05 LINE 13 COLUMN 15 VALUE "OPTION...: ".
             05 LINE 13 COLUMN 26     PIC X(001) TO WRK-OPTION.
-       
+
       *---------------------------- ERROR SCREEN
        01 ERROR-SCREEN.
             05 MSG-ERROR.
                 10 LINE 16 COLUMN 01 ERASE EOL
                    BACKGROUND-COLOR 3.
-                10 LINE 16 COLUMN 10 PIC X(030) 
-                   BACKGROUND-COLOR 3 
+                10 LINE 16 COLUMN 10 PIC X(030)
+                   BACKGROUND-COLOR 3
                    USING WRK-ERROR-MSG.
-                10 COLUMN PLUS 2     PIC X(001) 
+                10 COLUMN PLUS 2     PIC X(001)
                    BACKGROUND-COLOR 3
                    USING WRK-KEY.
 
@@ -108,7 +109,7 @@
 
        0200-INITIALIZE         SECTION.
             OPEN I-O MOVIES.
-            PERFORM 0210-VERIFICATION THRU 0220-DISPLAY-MENU.
+            PERFORM 0210-VERIFICATION.
 
        0210-VERIFICATION.
             EVALUATE MOVIES-STATUS
@@ -124,7 +125,10 @@
               WHEN OTHER
                 CONTINUE
             END-EVALUATE.
-            PERFORM 9000-MANAGE-ERROR.
+
+            IF WRK-ERROR-MSG NOT EQUAL SPACES
+               PERFORM 9000-MANAGE-ERROR
+            END-IF.
 
        0220-DISPLAY-MENU.
             DISPLAY CLEANER-SCREEN.
@@ -148,14 +152,14 @@
                  MOVE "MODULE - REPORT " TO WRK-MODULE-TITLE
                  CALL "REPORT" USING WRK-TITLE
               WHEN "X"
-                  PERFORM 0400-FINALIZE
+                  CONTINUE
               WHEN OTHER
                   MOVE WRK-MSG-OPTION TO WRK-ERROR-MSG
                   PERFORM 9000-MANAGE-ERROR
             END-EVALUATE.
 
-            MOVE SPACES TO MOVIES-TITLE MOVIES-GENRE MOVIES-DISTRIBUTOR 
-               WRK-ERROR-MSG.
+            MOVE SPACES TO MOVIES-TITLE MOVIES-GENRE MOVIES-DISTRIBUTOR
+               WRK-ERROR-MSG WRK-OPTION.
             MOVE ZEROS TO MOVIES-KEY MOVIES-DURATION MOVIES-RATING.
 
             PERFORM 0220-DISPLAY-MENU.
